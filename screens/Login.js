d920443f3443fdfe3dceb95ad25f27b2ff77d37d
paulozemr <pauloramosj213@gmail.com>
2023-04-25 16:28:58 -0300

    Pagina de login inicializada

--- screens/Login.js
+++ screens/Login.js
@@ -1,21 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
 
-export default function Login() {
+export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
+    
     <View style={styles.container}>
-      <StatusBar barStyle="light-content" />
-      <Text>Login</Text>
+      <LinearGradient
+          colors={['#212224', '#3b3c3d', '#212224']}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 0, y: 1 }}
+          style={{ flex: 1, position: 'absolute', left: 0, right: 0, top: 0, height: '100%' }}
+        />
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email."
+          onChangeText={(email) => setEmail(email)}
+        /> 
+      </View> 
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password."
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        /> 
+      </View> 
+      <TouchableOpacity style={styles.loginBtn}>
+       <Text style={styles.loginText}>LOGIN</Text> 
+      </TouchableOpacity> 
+    </View> 
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputView: {
+    backgroundColor: "#fff",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+    alignItems: "center",
+  },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    marginLeft: 10,
+  },
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#fff",
   },
 });
+
