b52517be977036d79bf0f8063b83b9086cf15c2d
Hypnos <101708968+GuilhermeTamanini@users.noreply.github.com>
2023-10-17 22:05:55 +0000

    Alteração na api utilizando MY_IP

--- api/usuario.js
+++
@@ -1,7 +0,0 @@
-// import axios from "axios";
-// export default class UsuariosApi {
-//   async buscarTodosOsUsuarios() {
-//     const response = await axios.get("http://192.52.55.47:19003/usuarios/");
-//     return response.data;
-//   }
-// } 
